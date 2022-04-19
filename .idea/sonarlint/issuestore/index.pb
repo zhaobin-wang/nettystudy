cc26097f92c81bc182e74668ab216ebe3e8ad28c
wangzhaobin <w185142767523@163.com>
2022-04-19 16:41:23 +0800

    记录

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,9 @@ h
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
 h
 8src/main/java/com/wangzhaobin/nettystudy/s01/Client.java,2/6/26660c755fe4e9f1a15383d7bb206ba6b78f0e8a
+M
+src/main/java/s01/Server.java,f/7/f7ef5df74ce4a92db32e5281ba75e76a61be2d18
+M
+src/main/java/s01/Client.java,d/7/d71d4a9c9b1f11b0d2eba5493544cb712a7ae9cc
+R
+"src/main/java/s02/ClientFrame.java,f/5/f5c5f28f344c69c4a179a8bfac239864168e1b4a
--- src/main/java/s02/ClientFrame.java
+++ src/main/java/s02/ClientFrame.java
@@ -1,8 +1,39 @@
 package s02;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
+ * 多人聊天
+ *
  * @author wangzhaobin
  * @date 2022/4/19 上午2:42
  */
-public class ClientFrame {
+public class ClientFrame extends Frame {
+
+    TextArea ta = new TextArea();
+    TextField tf = new TextField();
+
+    public ClientFrame(){
+        this.setSize(600,400);
+        this.setLocation(100,20);
+        this.add(ta, BorderLayout.CENTER);
+        this.add(tf, BorderLayout.SOUTH);
+
+        tf.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                //把字符串发送到服务器，服务器收到之后转发到所有的客户端
+                ta.setText(ta.getText() + tf.getText() + "\n");
+                tf.setText("");
+            }
+        });
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ClientFrame();
+    }
 }
